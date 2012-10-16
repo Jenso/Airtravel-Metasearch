301926f3270da5f92e848436e794898f2c4d77f7
Jenso <jenso1988@gmail.com>
2012-10-16 17:02:15 +0200

    Added bootstrap tags

---
+++ core/templatetags/tags.py
@@ -0,0 +1,22 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.simple_tag
+def bootstrap_less():
+    #<link rel="stylesheet/less" href="{{ STATIC_URL }}bootstrap.less">
+    #<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.0/less-1.3.0.min.js"></script>
+
+    if settings.DEVELOPMENT:
+        LESS_TAG = '<link rel="stylesheet/less" type="text/css" href="%s%s">'
+
+        output = [LESS_TAG % (settings.STATIC_URL, "bootstrap.less"),
+                  '<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.3.0/less-1.3.0.min.js" type="text/javascript"></script>',
+                  ]
+        output = '\n'.join(output)
+
+    else:
+        output = '<link charset="utf-8" rel="stylesheet" type="text/css" href="%scss/style.css">' % (settings.STATIC_URL)
+
+    return output
